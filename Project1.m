275a9b22104aa6564460811f650660c29eacb046
cgl1 <cgl1@txstate.edu>
2015-02-20 23:43:20 -0600

    Second changes to Matlab code for Project 1 -- Cody

--- Project1.m
+++ Project1.m
@@ -9,10 +9,14 @@ A = 3; f= 1000; omega = 2*pi*f;
 angle = degtorad(33); T=1/f;
 t=linspace(-3*T,3*T,1000);
 x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
+subplot(4,1,2)
 u_t=heaviside(t);
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
+c_t=heaviside(t-1);
+z_t = cos(omega*t).*sin(omega*t);
+subplot(4,1,4)
+plot(t,z_t.*c_t, 'linewidth',2);grid on
